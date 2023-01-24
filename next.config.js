1854e19536e13566bea8d093cf93a4924856d658
Junaid Malik <junmalik109@gmail.com>
2023-01-24 15:36:23 +0000

    Added domain origins for images

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['images.unsplash.com', 'res.cloudinary.com', 'bayut.com', 'bayut-production.s3.eu-central-1.amazonaws.com',
+  'https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'],
+  },
+
 }
 
 module.exports = nextConfig
